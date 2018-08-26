f14f3d42f6edbed8dc74d91eee6f3c5d95e78989
Matt Cliff <matt@denvercliffs.com>
2018-08-26 10:03:23 -0600

    permission updates and logging

--- README.md
+++ README.md
@@ -20,3 +20,12 @@ The API folder was created with the *serverless* tool.
 ## Build Notes
 
 we have a hook in github from the Code Pipeline in us-west-2
+
+WebHook in Git is
+`https://us-west-2.webhooks.aws/trigger?t=eyJlbmNyeXB0ZWREYXRhIjoiWklwZFM5TnJiaEFWM2NWR3VrenlCMGVIMHBoaE13Q0hISTBFVjlsOUt6QWZKUlNPZkJFbkJsM3o2d1VnOGlaaVlubUFoNGV5Y2ZaR2tDUjdYMnJ0UWRIRE5WTjNLTG5rcGJNNjFSalRVK2xka2dLdlRQZndWYmtEL2l1ZjQ5bi96cGZjWG9RRzRCa1MreWZFUDNwUUpRUjZBMWpHc21YVDJ1UU9kMllnU1lCTlRnPT0iLCJpdlBhcmFtZXRlclNwZWMiOiJ4azdOaXljaHh4WkxtalV3IiwibWF0ZXJpYWxTZXRTZXJpYWwiOjF9&v=1`
+
+This hook was created in **AWS CodePipeline**
+
+## TODO
+
+- Review and reduce permission sets on CodeBuild role to required assets
--- api/handler.py
+++ api/handler.py
@@ -6,7 +6,7 @@ import json
 from boto3.dynamodb.conditions import Key, Attr
 
 LOGGER = logging.getLogger()
-LOGGER.setLevel(logging.INFO)
+LOGGER.setLevel(logging.DEBUG)
 
 """
 expects to have  string object 'postId' passed in as data
@@ -48,10 +48,13 @@ event should have parameters voice and text that
 def handle_post(event):
     method = 'handle_post():'
     LOGGER.debug('%sbegin', method)
+
     data = json.loads(event['body'])
     recordId = str(uuid.uuid4())
     voice = data["voice"]
     text = data["text"]
+    LOGGER.debug('%sinsert:{id=%s, text=%s, voice=%s}', method, recordId, text, voice)
+
     #Creating new record in DynamoDB table
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(os.environ['DB_TABLE_NAME'])
