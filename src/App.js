a965f29974a4962cc7fded753967144d482e5cf0
Matt Cliff <matt@denvercliffs.com>
2018-11-03 17:26:00 -0600

    imported appnav

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route  } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
+import AppNav from './AppNav';
 
 class App extends Component {
 
