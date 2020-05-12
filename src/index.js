f5f8b2c62d0c29b92bc3fff00b5a0a345a110731
Matt Cliff <matt@denvercliffs.com>
2020-05-11 21:13:39 -0600

    updates to core packaeges

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-require('dotenv').config();
-console.log('TODO is dotenv.config necessary: %o', process.env);
+//require('dotenv').config();
+//console.log('ok TODO is dotenv.config necessary: %o', process.env);
+
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>
   , document.getElementById('root'));
-  registerServiceWorker();
+  //registerServiceWorker();
